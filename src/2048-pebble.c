8b6194bb296dce641295b7b8381c8734cb199c65
crazy2be <code@jmcgirr.com>
2014-03-14 12:59:42 -0400

    Basic accell working.

--- src/2048-pebble.c
+++ src/2048-pebble.c
@@ -1,41 +1,47 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static Layer *game_layer;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
-}
-
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+//   text_layer_set_text(text_layer, "Select");
 }
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+static void game_layer_update_callback(Layer *me, GContext *ctx) {
+  AccelData accel = {0};
+  accel_service_peek(&accel);
+//   graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_draw_circle(ctx, GPoint(144/2, 144/2), 10);
+  graphics_fill_circle(ctx, GPoint(accel.x/10 + 144/2, -accel.y/10 + 144/2), (accel.z + 4000)/200);
+
+}
+
+static void timer_callback(void *data) {
+  layer_mark_dirty(game_layer);
+  app_timer_register(100, timer_callback, NULL);
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  game_layer = layer_create(bounds);
+  layer_add_child(window_layer, game_layer);
+  layer_set_update_proc(game_layer, game_layer_update_callback);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  layer_destroy(game_layer);
 }
 
 static void init(void) {
+  accel_data_service_subscribe(0, NULL);
+  app_timer_register(100, timer_callback, NULL);
+
   window = window_create();
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
@@ -48,6 +54,7 @@ static void init(void) {
 
 static void deinit(void) {
   window_destroy(window);
+  accel_data_service_unsubscribe();
 }
 
 int main(void) {
