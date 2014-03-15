8fe5a1256581e4e73c175c888fdd39e2e875f836
crazy2be <code@jmcgirr.com>
2014-03-15 17:56:37 -0400

    Add pause.

--- src/accel.c
+++ src/accel.c
@@ -8,8 +8,7 @@ static const GPathInfo TRIANGLE_PATH_INFO = {
 static GPath *s_triangle_path = NULL;
 static GPath *s_triangle_strength[10] = {NULL};
 
-static bool s_select_down = false;
-static Direction s_current_direction;
+static bool s_input_paused = true;
 
 void accel_init() {
   s_triangle_path = gpath_create(&TRIANGLE_PATH_INFO);
@@ -30,8 +29,13 @@ static void back_click_handler(ClickRecognizerRef recognizer, void *context) {
   game_undo();
 }
 
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  s_input_paused = !s_input_paused;
+}
+
 void accel_click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
 static int16_t abs_int16(int16_t n) {
@@ -86,9 +90,9 @@ static Direction read_accel_direction(AccelData accel) {
 
 static int read_accel_strength(AccelData accel) {
   if (abs_int16(accel.x) > abs_int16(accel.y)) {
-    return abs_int16(accel.x);
+    return abs_int16(accel.x)/10;
   } else {
-    return abs_int16(accel.y);
+    return abs_int16(accel.y)/15;
   }
 }
 
@@ -99,36 +103,67 @@ void draw_all_directions(GContext *ctx) {
   }
 }
 
-static Direction s_last_direction = DIRECTION_NONE;
+static void draw_accel_debug(GContext* ctx, AccelData accel) {
+  graphics_draw_circle(ctx, GPoint(144/2, 144/2 + (168-144)), 10);
+  graphics_fill_circle(ctx, GPoint(accel.x/10 + 144/2, -accel.y/10 + 144/2 + (168-144)), (accel.z + 4000)/200);
+}
+
+static void draw_accel_arrows(GContext* ctx, Direction dir, int strength) {
+  if (strength == 0) {
+    draw_all_directions(ctx);
+    return;
+  }
+  transform_path(s_triangle_path, dir, GPoint(0, 0));
+  gpath_draw_outline(ctx, s_triangle_path);
+  for (int i = 0; i < 10; i++) {
+    if (strength < i + 1) {
+      continue;
+    }
+    transform_path(s_triangle_strength[i], dir, GPoint(0, 0));
+    gpath_draw_outline(ctx, s_triangle_strength[i]);
+  }
+}
+
+static void draw_paused_glyph(GContext* ctx) {
+  int h = 50;
+  int w = 20;
+  int gap = 12;
+  graphics_fill_rect(ctx, GRect(144/2-w-gap/2, 168/2-h/2, w, h), 0, GCornersAll);
+  graphics_fill_rect(ctx, GRect(144/2+gap/2, 168/2-h/2, w, h), 0, GCornersAll);
+}
+
+static Direction s_last_move_direction = DIRECTION_NONE;
+static Direction s_last_sample_direction = DIRECTION_NONE;
+static int s_last_sample_strength = 0;
+
 void accel_draw(GContext *ctx) {
+  if (s_input_paused) {
+    draw_accel_arrows(ctx, s_last_sample_direction, s_last_sample_strength);
+    draw_paused_glyph(ctx);
+    return;
+  }
+
   AccelData accel = {0};
   accel_service_peek(&accel);
 
-  graphics_draw_circle(ctx, GPoint(144/2, 144/2 + (168-144)), 10);
-  graphics_fill_circle(ctx, GPoint(accel.x/10 + 144/2, -accel.y/10 + 144/2 + (168-144)), (accel.z + 4000)/200);
-
-  if ((abs_int16(accel.x) < 50 && abs_int16(accel.y) < 50)) {
-    draw_all_directions(ctx);
-    s_last_direction = DIRECTION_NONE;
+  if (accel.did_vibrate) {
     return;
   }
 
   Direction dir = read_accel_direction(accel);
   int strength = read_accel_strength(accel);
 
-  transform_path(s_triangle_path, dir, GPoint(0, 0));
-  gpath_draw_outline(ctx, s_triangle_path);
-  for (int i = 0; i < 10; i++) {
-    if (strength < (i+1)*50) continue;
-    transform_path(s_triangle_strength[i], dir, GPoint(0, 0));
-    gpath_draw_outline(ctx, s_triangle_strength[i]);
+//   draw_accel_debug(ctx, accel);
+  draw_accel_arrows(ctx, dir, strength);
+
+  if (strength >= 10 && dir != s_last_move_direction) {
+    game_move(dir);
+    s_last_move_direction = dir;
+  } else if (strength <= 7) {
+    s_last_move_direction = DIRECTION_NONE;
   }
 
-  if (strength / 50 >= 10 && dir != s_last_direction) {
-    game_move(dir);
-    s_last_direction = dir;
-  } else if (strength / 50 <= 7) {
-    s_last_direction = DIRECTION_NONE;
-  }
+  s_last_sample_direction = dir;
+  s_last_sample_strength = strength;
 }
 
