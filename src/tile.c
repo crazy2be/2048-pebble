c11097dc23a25890178517fdddbecc979581f0da
crazy2be <code@jmcgirr.com>
2014-03-25 13:32:55 -0400

    Remove unused code.

--- src/tile.c
+++ src/tile.c
@@ -7,105 +7,38 @@ static const int BORDER_SIZE = 12;
 static GBitmap* s_tiles_bitmap = NULL;
 static GBitmap* s_tile_bitmap[TILE_MAX_VAL] = {NULL};
 
-void tile_draw_glyph(GContext* ctx, int xs, int ys, int val) {
-  if (s_tiles_bitmap == NULL) {
-    s_tiles_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TILES_BITMAP);
-  }
+void tile_draw(GContext *ctx, GPoint origin, int val) {
   if (val > TILE_MAX_VAL) {
     APP_LOG(APP_LOG_LEVEL_ERROR, "Val %d out of bounds! Ignoring", val);
     return;
   }
-  if (s_tile_bitmap[val] == NULL) {
-    int s = TILE_SIZE;
-    int col = val % 4;
-    int row = val / 4;
-    GRect bitmap_rect = GRect(col*s + 1, row*s + 1, s - 2, s - 2);
-    s_tile_bitmap[val] = gbitmap_create_as_sub_bitmap(s_tiles_bitmap, bitmap_rect);
-  }
 
-  GRect screen_rect = GRect(xs + 1, ys + 1, TILE_SIZE - 2, TILE_SIZE - 2);
-//   graphics_context_set_compositing_mode(ctx, GCompOpAnd);
-  graphics_draw_bitmap_in_rect(ctx, s_tile_bitmap[val], screen_rect);
-}
-
-void tile_draw_rects(GContext* ctx, int xs, int ys, int val) {
-  val++;
-  if (val < 7) {
-    for (int i = val; i > 0; i--) {
-      if ((val - i) % 2 == 1) continue;
-      graphics_draw_rect(ctx, (GRect) {
-        .origin = GPoint(xs + TILE_SIZE/2 - i, ys + TILE_SIZE/2 - i),
-        .size = GSize(i*2, i*2),
-      });
-    }
-  } else if (val < 9) {
-    for (int i = val; i > 0; i--) {
-      if (i % 3 == 0) continue;
-      graphics_draw_rect(ctx, (GRect) {
-        .origin = GPoint(xs + TILE_SIZE/2 - i, ys + TILE_SIZE/2 - i),
-        .size = GSize(i*2, i*2),
-      });
-    }
-  } else if (val < 11) {
-    for (int i = val; i > 0; i--) {
-      if (i % 4 == 0) continue;
-      graphics_draw_rect(ctx, (GRect) {
-        .origin = GPoint(xs + TILE_SIZE/2 - i, ys + TILE_SIZE/2 - i),
-        .size = GSize(i*2, i*2),
-      });
-    }
-  } else {
-    for (int i = val; i > 0; i--) {
-      if (i % 5 == 0) continue;
-      graphics_draw_rect(ctx, (GRect) {
-        .origin = GPoint(xs + TILE_SIZE/2 - i, ys + TILE_SIZE/2 - i),
-        .size = GSize(i*2, i*2),
-      });
-    }
-  }
-}
-
-void tile_draw_text(GContext* ctx, int xs, int ys, int val) {
-  static char score_buf[100];
-  snprintf(score_buf, 100, "%d", 1 << val);
-
-  graphics_context_set_text_color(ctx, GColorBlack);
-  graphics_draw_text(ctx,
-                     score_buf,
-                     fonts_get_system_font(FONT_KEY_FONT_FALLBACK),
-                     GRect(xs + 1, ys + 5, TILE_SIZE - 2, TILE_SIZE - 2),
-                     GTextOverflowModeWordWrap,
-                     GTextAlignmentCenter,
-                     NULL);
-}
-
-static long unsigned int ms_time() {
-  return time_ms(NULL, NULL) + time(NULL)*1000;
-}
-
-typedef void (*DrawMethod)(GContext*, int, int, int);
-
-void profile_draw_method(GContext* ctx, int xs, int ys, int val, DrawMethod func, const char* name) {
-  long unsigned int start_time = ms_time();
-  for (int i = 0; i < 100; i++) {
-    func(ctx, xs, ys, val);
-  }
-  long unsigned int end_time = ms_time();
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Running method %s took %lu ms", name, end_time - start_time);
-}
-
-void tile_draw(GContext *ctx, GPoint origin, int val) {
+  // We don't draw "empty" tiles, so that you can see the "ground" under
+  // them when tiles are animating.
   if (val == 0) return;
 
   int xs = origin.x;
   int ys = origin.y;
 
-//   graphics_draw_rect(ctx, GRect(xs + 1, ys + 1, TILE_SIZE - 2, TILE_SIZE - 2));
+  if (s_tiles_bitmap == NULL) {
+    s_tiles_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TILES_BITMAP);
+  }
 
-  tile_draw_glyph(ctx, xs, ys, val);
-//   profile_draw_method(ctx, xs, ys, val, tile_draw_glyph, "glpyh");
-//   profile_draw_method(ctx, xs, ys, val, tile_draw_rects, "rects");
-//   profile_draw_method(ctx, xs, ys, val, tile_draw_text, "text");
+  if (s_tile_bitmap[val] == NULL) {
+    int s = TILE_SIZE;
+    int col = val % 4;
+    int row = val / 4;
+    // Leaving a little bit of a gap between tiles ensures you can always
+    // see tile animations. Ideally this would be represented by transparency
+    // in the PNG, but that needs more memory, and doesn't seem to be
+    // natively supported by the Pebble SDK. So we just ignore a few pixels
+    // instead.
+    GRect bitmap_rect = GRect(col*s + 1, row*s + 1, s - 2, s - 2);
+    s_tile_bitmap[val] = gbitmap_create_as_sub_bitmap(s_tiles_bitmap, bitmap_rect);
+  }
+
+  GRect screen_rect = GRect(xs + 1, ys + 1, TILE_SIZE - 2, TILE_SIZE - 2);
+  graphics_draw_bitmap_in_rect(ctx, s_tile_bitmap[val], screen_rect);
 }
 
 GPoint tile_position(Cell cell) {
